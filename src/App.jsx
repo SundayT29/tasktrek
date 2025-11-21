3c7a57a9f81e294023149af9502e0c418a69b5b6
Timothy Adebisi <adebisis539@gmail.com>
2025-11-21 13:40:33 +0100

    task form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+
 import "./App.css";
+import TaskForm from "./components/TaskForm";
+import TaskColumn from "./components/TaskColumn";
+import todoIcon from "./assets/direct-hit.png";
+import doingIcon from "./assets/glowing-star.png";
+import doneIcon from "./assets/check-mark-button.png";
 
 const App = () => {
   return (
     <div className="app">
-      <header className="app_header">Header Section</header>
+      <TaskForm />
       <main className="app_main">
-        <section className="task_column">Section 1</section>
-        <section className="task_column">Section 2</section>
-        <section className="task_column">Section 3</section>
+        <TaskColumn title="To do" icon={todoIcon} status="todo" />
+        <TaskColumn title="Doing" icon={doingIcon} status="doing" />
+        <TaskColumn title="Done" icon={doneIcon} status="done" />
       </main>
     </div>
   );
